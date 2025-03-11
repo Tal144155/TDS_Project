fe067770b327664e77535aaa8ca7e9f1ab73412d
Tal144155 <tal.ariel.ziv@gmail.com>
2025-03-11 15:00:21 +0200

    added comments

--- Final Project/research_system.py
+++ Final Project/research_system.py
@@ -66,93 +66,71 @@ def switch_to_main():
     main_frame.pack(expand=True)
     start_process()
 
-# Button to submit feedback
-def submit_feedback():
-    global plot_index, plot_data, start_time, ratings
-    rating = int(rating_var.get()) if rating_var.get().isdigit() else None
-    comment = comment_box.get("1.0", tk.END).strip()
-    if not rating or rating < 1 or rating > 5:
-        messagebox.showerror("Invalid Input", "Please provide a rating between 1 and 5.")
-        return
-    if not comment:
-        messagebox.showerror("Invalid Input", "Please provide a comment.")
-        return
-    if is_system_plot:
-        user_rating = 0
-        if pd.notna(ratings.loc[user_id, chosen_plot['relation_type']]):
-            user_rating = ratings.loc[user_id, chosen_plot['relation_type']]
-        if user_rating:
-            ratings.loc[user_id, chosen_plot['relation_type']] = user_rating * 0.5 + rating * 0.5
-        else:
-            ratings.loc[user_id, chosen_plot['relation_type']] = rating
-
-        save_ratings(ratings, 'user_ratings_rel2') 
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    plot_info = plot_data[plot_index]
-    plot_info.update({
-        'rating': rating,
-        'comment': comment,
-        'time_taken': elapsed_time
-    })
-    ratings = load_ratings('user_ratings_rel2', RELATION_TYPES)
-    plot_index += 1
-    if algo_rec and plot_index < min(len(algo_rec), 10) + 5:
-        show_next_plot()
-    else:
-        save_results()
-        messagebox.showinfo("Process Complete", "Thank you! The process is complete.")
-        app.quit()
-
 def start_process():
+    # global variables needed
     global start_time,num_chose_outliers, user_id, ratings, plot_index, algo_rec, df, dataset_types, count_system, dataset_path, plot_order
+    # asking for the user id to enter the system
     user_id = simpledialog.askstring("User ID", "Please enter your User ID:")
     if not user_id:
         messagebox.showerror("Invalid Input", "User ID is required to start.")
         return
-    ratings = load_ratings('user_ratings_rel2', RELATION_TYPES)
+    # load the ratings files
+    ratings = load_ratings('user_ratings', RELATION_TYPES)
     if user_id not in ratings.index:
         ratings.loc[user_id] = np.nan
-        save_ratings(ratings, 'user_ratings_rel2')
+        save_ratings(ratings, 'user_ratings')
     plot_index = 0
     count_system = 0
     num_chose_outliers = 0
     dataset_path = "Final Project/Datasets_Testing/movie_new.csv"
     index_col = "id"
     target_value = "revenue"
+    # upload the data and start the process
     df = read_data(dataset_path)
     if df is None:
         return
+    # get the types and find the relations, then get the top ones
     dataset_types = get_column_types(df)
     algo_rec = find_relations(df, target_value, dataset_types)
     algo_rec = get_relation_scores(algo_rec)
-
+    # define number of plots about to be shown
     num_system_plots = min(10, len(algo_rec))
     num_random_plots = 5
-
+    # create an array with the order of displaying plots.
     plot_order = [True] * num_system_plots + [False] * num_random_plots
+    # shuffle the array for random order
     random.shuffle(plot_order)
 
     show_next_plot()
 
 def generate_plot():
+    """This function is responsible for creating a new visualization, system or random,
+    depends on the plot number"""
     global user_id, algo_rec, chosen_plot, is_system_plot, count_system, num_chose_outliers
+    # if the plot is in the boundaries of the array, get it's type from the array
     if plot_index < len(plot_order):
         is_system_plot = plot_order[plot_index]
     else:
         is_system_plot = False
+    # create the name of the plot
     plot_name = f'plot{plot_index+1}'
-    image_path = ""
-    ratings = load_ratings('user_ratings_rel2', RELATION_TYPES)
+    # load the ratings
+    ratings = load_ratings('user_ratings', RELATION_TYPES)
+    # if it is a system plot
     if is_system_plot and algo_rec:
+        # get the most wanted visualization by the user
         count_system = count_system + 1
         combined_user_vis_pred = combine_pred(CFIB(ratings), CFUB(ratings), 0.5, 0.5)
         algo_rec_df = get_top_relations(algo_rec)
         user_index = ratings.index.get_loc(user_id)
         recommendations = combine_pred(combined_user_vis_pred[user_index], algo_rec_df.to_numpy()[0], 0.7, 0.3)
         index = int(algo_rec_df.iloc[1,recommendations.argmax()])
+        # take it out of the list
         chosen_plot = algo_rec.pop(index)
+        # if the chosen plot is the outlier (which is shown a lot) we want it to be only twice.
+        # if it is more then 2 times, choose another one instead
         if chosen_plot['relation_type'] == "outlier_pattern" and num_chose_outliers >= 2:
+            # get other relations instead until we get the relation that is not outlier
             while chosen_plot['relation_type'] == "outlier_pattern" and algo_rec:
                 combined_user_vis_pred = combine_pred(CFIB(ratings), CFUB(ratings), 0.5, 0.5)
                 algo_rec_df = get_top_relations(algo_rec)
@@ -160,13 +138,16 @@ def generate_plot():
                 recommendations = combine_pred(combined_user_vis_pred[user_index], algo_rec_df.to_numpy()[0], 0.7, 0.3)
                 index = int(algo_rec_df.iloc[1,recommendations.argmax()])
                 chosen_plot = algo_rec.pop(index)
+        # otherwise present this visualization
         elif chosen_plot['relation_type'] == "outlier_pattern":
             num_chose_outliers += 1
+        # update the name of the plot with the number of features.
         if len(chosen_plot["attributes"]) == 1:
             plot_name = f'{chosen_plot["relation_type"]} between {chosen_plot["attributes"][0]}'
         else:
             plot_name = f'{chosen_plot["relation_type"]} between {chosen_plot["attributes"][0]}, {chosen_plot["attributes"][1]}'
         plot_save_name = f'plot_{plot_index}'
+        # create the info of the plot with the important information
         plot_info = {
             'name': plot_name,
             'is_system': True,
@@ -174,6 +155,7 @@ def generate_plot():
             'attributes': chosen_plot["attributes"],
             'plot_save_name': plot_save_name
         }
+        # based on the relation type, call the appropriate method from plot generator
         if chosen_plot['relation_type'] == "high_correlation":
             plot_high_correlation(df, chosen_plot["attributes"][0], chosen_plot["attributes"][1], chosen_plot["details"]["correlation_value"], plot_save_name)
         elif chosen_plot['relation_type'] == "target_correlation":
@@ -194,10 +176,12 @@ def generate_plot():
             plot_outlier_pattern(df, chosen_plot["attributes"][0], chosen_plot["attributes"][1], plot_save_name)
         elif chosen_plot['relation_type'] == "target_analysis":
             plot_target_analysis(df, chosen_plot["attributes"][0], chosen_plot["details"]["outlier_ratio"], chosen_plot["details"]["distribution_type"], plot_save_name)
-
+        # create the image path
         image_path = os.path.abspath(os.path.join(PLOTS_DIR, f'{plot_save_name}.png'))
     else:
+        # otherwise, we want to create a random visualization, and search for 2 random features
         allowed_types = {"integer", "float", "datetime", "categorical_int", "categorical_string"}
+        # run until you find 2 features from the list
         while True:
             selected_features = random.sample(list(dataset_types.keys()), 2)
             feature1, feature2 = selected_features[0], selected_features[1]
@@ -206,6 +190,7 @@ def generate_plot():
             
             if feature1_type in allowed_types and feature2_type in allowed_types:
                 break
+        # go over the same process as before
         plot_name = f'{feature1} {feature2}'
         plot_save_name = f'plot_{plot_index}'
         plot_info = {
@@ -213,7 +198,7 @@ def generate_plot():
             'plot_save_name': plot_save_name,
             'attributes': [feature1, feature2]
         }
-        # Handling different feature type combinations
+        # Handling different feature type combinations, there can be a lot of combinations and we want to handle them all
         if (feature1_type == "integer" or feature1_type == "float") and (feature2_type == "integer" or feature2_type == "float"):
             plot_type = random.choice(['high_correlation', 'non_linear'])
             if plot_type == "high_correlation":
@@ -262,17 +247,21 @@ def generate_plot():
             plot_info["name"] = f'{plot_info["relation_type"]} between {feature1}, {feature2}'
 
         image_path = os.path.join(PLOTS_DIR, f'{plot_save_name}.png')
+    # adding the info to the plot data
     plot_data.append(plot_info)
+    # displaying the plot on the screen
     display_plot(image_path)
     return plot_info
 
 
 def display_plot(image_path):
+    # check that the image exists in the folder
     if not os.path.exists(image_path):
         messagebox.showerror("Error", f"Image not found: {image_path}")
         return
 
     try:
+        # trying to present the plot for the user.
         image = Image.open(image_path)
         max_width, max_height = 600, 400
         image.thumbnail((max_width, max_height), Image.Resampling.LANCZOS)
@@ -289,16 +278,64 @@ def display_plot(image_path):
 
 # Function to display the next plot
 def show_next_plot():
+    # generate the plot for the user to see.
     global start_time
     plot_info = generate_plot()
     plot_label.config(text=f"Plot: {plot_info['name']}")
     comment_box.delete("1.0", tk.END)
     rating_var.set("")
     time.sleep(1)
+    # start the time after the plot is shown
     start_time = time.time()
+
+# Button to submit feedback
+def submit_feedback():
+    global plot_index, plot_data, start_time, ratings
+    rating = int(rating_var.get()) if rating_var.get().isdigit() else None
+    comment = comment_box.get("1.0", tk.END).strip()
+    if not rating or rating < 1 or rating > 5:
+        messagebox.showerror("Invalid Input", "Please provide a rating between 1 and 5.")
+        return
+    if not comment:
+        messagebox.showerror("Invalid Input", "Please provide a comment.")
+        return
+    # only update the rating if its a system plot.
+    if is_system_plot:
+        user_rating = 0
+        if pd.notna(ratings.loc[user_id, chosen_plot['relation_type']]):
+            user_rating = ratings.loc[user_id, chosen_plot['relation_type']]
+        if user_rating:
+            # update with weight to the previews rating of this kind.
+            ratings.loc[user_id, chosen_plot['relation_type']] = user_rating * 0.5 + rating * 0.5
+        else:
+            ratings.loc[user_id, chosen_plot['relation_type']] = rating
+        save_ratings(ratings, 'user_ratings') 
+    end_time = time.time()
+    # calculate the time took to submit the rating and comments
+    elapsed_time = end_time - start_time
+    plot_info = plot_data[plot_index]
+    # adding the information to the plot info
+    plot_info.update({
+        'rating': rating,
+        'comment': comment,
+        'time_taken': elapsed_time
+    })
+    # load the new ratings
+    ratings = load_ratings('user_ratings', RELATION_TYPES)
+    plot_index += 1
+    # if we have more plots to show and we did not finish 15 plots, show a new one
+    if algo_rec and plot_index < min(len(algo_rec), 10) + 5:
+        show_next_plot()
+    else:
+        # save the results and end the process
+        save_results()
+        messagebox.showinfo("Process Complete", "Thank you! The process is complete.")
+        app.quit()
+
 
 # Function to save results to a text file
 def save_results():
+    # save the results after the user finished rating all the plots
     results_file = "user_feedback.txt"
     with open(results_file, 'w') as f:
         f.write(f"Results on test from dataset: {dataset_path}\n")
